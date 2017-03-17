c005085c22ba488a339fc35e34f6853d4dcd4179
Anıl Gülgör <anilgulgor@Anl-MacBook-Air.local>
2017-03-17 08:55:28 +0300

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+
+        return console.log('Unable to connect mongodb server');
+
+    }
+
+    console.log('Connected to mongodb server');
+
+    /*db.collection('Todos').find({_id : new ObjectID('58c925eec09a2d6188bfb03f')}).toArray().then((docs) => {
+
+        console.log('Todos');
+
+        console.log(JSON.stringify(docs, undefined, 2));
+
+    }, (err) => {
+
+        console.log('Unable to find documents');
+
+    });*/
+
+    db.collection('Todos').count().then((count) => {
+
+        console.log(`Todos count : ${count}`);
+
+    }, (err) => {
+
+        console.log('Unable to count collection');
+
+    });
+
+    //db.close();
+
+});
